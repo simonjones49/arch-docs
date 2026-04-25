73d221d734ac087855c63b532487ddb24c163c86
Simon Jones <simon_jones49@gmx.com>
2026-04-25 14:56:20 +0200

    Deployed 5f43027 with MkDocs version: 1.6.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -3,17 +3,3 @@ document.querySelectorAll(".md-social__link").forEach((link) => {
   link.setAttribute("target", "_blank");
   link.setAttribute("rel", "noopener");
 });
-// Force the 'Back to Top' button to stay visible once scrolled
-window.addEventListener("scroll", function() {
-  var topButton = document.querySelector(".md-top");
-  if (topButton) {
-    if (window.scrollY > 200) { // Shows after 200px of scrolling
-      topButton.setAttribute("data-md-state", "active");
-      topButton.style.hidden = false;
-      topButton.style.opacity = "1";
-    } else {
-      topButton.setAttribute("data-md-state", "");
-      topButton.style.opacity = "0";
-    }
-  }
-});
