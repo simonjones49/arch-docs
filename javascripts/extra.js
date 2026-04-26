40cb44da8b841e0e4c36f31a8c17032622411cbc
Simon Jones <simon_jones49@gmx.com>
2026-04-26 17:32:25 +0200

    Deployed 4bd2c77 with MkDocs version: 1.6.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -3,3 +3,9 @@ document.querySelectorAll(".md-social__link").forEach((link) => {
   link.setAttribute("target", "_blank");
   link.setAttribute("rel", "noopener");
 });
+var consent = __md_get("__consent")
+if (consent && consent.custom) {
+  /* The user accepted the cookie */
+} else {
+  /* The user rejected the cookie */
+}
